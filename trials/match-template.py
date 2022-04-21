18be0add78a6f150afb27d290b482c1f67f96bc7
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-04-21 15:43:56 +1200

    Can detect multiple instances of the template image, however need to improve detection accuracy

--- trials/match-template.py
+++ trials/match-template.py
@@ -7,7 +7,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Importing input images
-input_image = cv.imread(LOCAL_PATH + INPUT_IMAGE_PATH,0)
+input_image = cv.imread(LOCAL_PATH + INPUT_IMAGE_PATH)
+input_image_gray = cv.cvtColor(input_image, cv.COLOR_BGR2GRAY)
 input_image_copy = input_image.copy()
 
 # Importing template images
@@ -24,22 +25,36 @@ for method in methods:
     img = input_image_copy.copy()
     current_method = eval(method)
 
-    # Apply template Matching
-    result = cv.matchTemplate(img, template_image, current_method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    # Thresholding for when there's multiple template image instances in the input image to search for
+    result = cv.matchTemplate(input_image_gray, template_image, cv.TM_CCOEFF_NORMED)
+    threshold = 0.48
+    loc = np.where(result >= threshold)
+    for pt in zip(*loc[::-1]):
+      cv.rectangle(input_image, pt, (pt[0] + template_width, pt[1] + template_height), (0, 0, 255), 2)
+    cv.imwrite('result.png', input_image)
 
-    # If the current_method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if current_method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-
-    bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(result,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(method)
-    plt.show()
+    # cv.minMaxLoc function for when there's one template image instance in the input image to search for
+    # If the above is true, then below code must be commented out
+    # [ONE TEMPLATE IMAGE INSTANCE CODE STARTING HERE]
+    #
+    # # Apply template Matching
+    # result = cv.matchTemplate(img, template_image, current_method)
+    #
+    # min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    #
+    # # If the current_method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    # if current_method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+    #     top_left = min_loc
+    # else:
+    #     top_left = max_loc
+    #
+    # bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
+    # cv.rectangle(img,top_left, bottom_right, 255, 2)
+    # plt.subplot(121),plt.imshow(result,cmap = 'gray')
+    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    # plt.suptitle(method)
+    # plt.show()
+    # [ONE TEMPLATE IMAGE INSTANCE CODE ENDING HERE]
 
