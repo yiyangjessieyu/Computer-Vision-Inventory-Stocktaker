648044a91d256c85a872cd381d1c00bee92e4dab
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-05-26 13:48:49 +1200

    used hough_lines to detect correct angles

--- src/detect_morph_lines.py
+++ src/detect_morph_lines.py
@@ -55,11 +55,40 @@ def transform_grey(src):
     else:
         return src
 
+def transform_bitwise(src):
+    # Apply adaptiveThreshold at the bitwise_not of gray, notice the ~ symbol
+    src = cv.bitwise_not(src)
+    bitwise = cv.adaptiveThreshold(src, 255, cv.ADAPTIVE_THRESH_MEAN_C, \
+                                cv.THRESH_BINARY, 15, -2)
+
+    return bitwise
+
+def extract_vertical(bitwise):
+    # Create the images that will use to extract the vertical lines
+    vertical = np.copy(bitwise)
+
+    # Specify size on vertical axis
+    rows = vertical.shape[0]
+    vertical_size = rows // 30
+
+    # Create structure element for extracting vertical lines through morphology operations
+    verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, vertical_size))
+    # show_wait_destroy("verticalStructure", verticalStructure)
+
+    # Apply morphology operations
+    vertical = cv.erode(vertical, verticalStructure)
+    vertical = cv.dilate(vertical, verticalStructure, iterations=5)
+
+    return vertical
+
 def main(argv):
 
     # [load_image]
     src = read_image(argv)
     cv.imshow("src", src)
+
+    # [hough lines]
+    src = extract_hough_normal(src)
 
     # [gray]
     gray = transform_grey(src)
@@ -69,44 +98,19 @@ def main(argv):
     # show_wait_overlay(src, cv.imread(gray))
 
 
-    # [gray]
-    # [bin]
-    # Apply adaptiveThreshold at the bitwise_not of gray, notice the ~ symbol
-    gray = cv.bitwise_not(gray)
-    bw = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, \
-                                cv.THRESH_BINARY, 15, -2)
-    # Show binary image
-    show_wait_destroy("binary", bw)
+    # [bitwise]
+    bitwise = transform_bitwise(gray)
+    show_wait_destroy("bitwise", bitwise)
 
-    # [bin]
-    # [init]
-    # Create the images that will use to extract the vertical lines
-    vertical = np.copy(bw)
-
-    # [vert]
-    # Specify size on vertical axis
-    rows = vertical.shape[0]
-    verticalsize = rows // 30
-
-    # Create structure element for extracting vertical lines through morphology operations
-    verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, verticalsize))
-    #show_wait_destroy("verticalStructure", verticalStructure)
-
-    # Apply morphology operations
-    vertical = cv.erode(vertical, verticalStructure)
-    vertical = cv.dilate(vertical, verticalStructure, iterations=5)
-    show_wait_destroy("vertical", vertical)
-    vertical = cv.morphologyEx(vertical, cv.MORPH_CLOSE, verticalStructure)
-
-    # Show extracted vertical lines
+    # [vertical]
+    vertical = extract_vertical(bitwise)
     show_wait_destroy("vertical", vertical)
 
-    # [vert]
+    # [inverse bitwise]
+    vertical_inverse = cv.bitwise_not(vertical)
+    show_wait_destroy("vertical_inverse", vertical_inverse)
+
     # [smooth]
-    # Inverse vertical image
-    vertical = cv.bitwise_not(vertical)
-    show_wait_destroy("vertical_bit", vertical)
-
     '''
     Extract edges and smooth image according to the logic
     1. extract edges
@@ -139,6 +143,78 @@ def main(argv):
     show_wait_destroy("smooth - final", vertical)
     # [smooth]
     return 0
+
+
+def extract_hough_normal(src):
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+    #gray = convertToGrayBlur(img_original);
+
+    #gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+    #gray = cv2.morphologyEx(gray, cv2.MORPH_DILATE, np.ones((7,7),np.uint8))
+
+    cv.namedWindow('Hough Line Transform')
+    cv.createTrackbar('CannyThreshold1', 'Hough Line Transform', 0, 1200, nothing)
+    cv.createTrackbar('CannyThreshold2', 'Hough Line Transform', 200, 1200, nothing)
+    cv.createTrackbar("HoughThreshold", 'Hough Line Transform', 240, 1200, nothing)
+
+    while True:
+        houghThreshold = cv.getTrackbarPos('HoughThreshold', 'Hough Line Transform')
+        cannyThreshold1 = cv.getTrackbarPos('CannyThreshold1', 'Hough Line Transform')
+        cannyThreshold2 = cv.getTrackbarPos('CannyThreshold2', 'Hough Line Transform')
+
+
+        # Create a new copy of the original image for drawing on later.
+        img = src.copy()
+        # Use the Canny Edge Detector to find some edges.
+        edges = cv.Canny(gray, cannyThreshold1, cannyThreshold2)
+        # Attempt to detect straight lines in the edge detected image.
+        lines = cv.HoughLines(edges, 1, np.pi/180, houghThreshold)
+
+        dark = np.zeros(img.shape)
+
+        # For each line that was detected, draw it on the img.
+        if lines is not None:
+            for line in lines:
+                for rho,theta in line:
+                    if (theta >= np.radians(0) and theta <= np.radians(10)) or \
+                        (theta >= np.radians(350) and theta <= np.radians(360)) or \
+                        (theta >= np.radians(170) and theta <= np.radians(190)) :
+                        a = np.cos(theta)
+                        b = np.sin(theta)
+                        x0 = a*rho
+                        y0 = b*rho
+                        x1 = int(x0 + 1000*(-b))
+                        y1 = int(y0 + 1000*(a))
+                        x2 = int(x0 - 1000*(-b))
+                        y2 = int(y0 - 1000*(a))
+
+                        cv.line(dark,(x1,y1),(x2,y2),(0,255,0),2)
+                        cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    else:
+                        a = np.cos(theta)
+                        b = np.sin(theta)
+                        x0 = a*rho
+                        y0 = b*rho
+                        x1 = int(x0 + 1000*(-b))
+                        y1 = int(y0 + 1000*(a))
+                        x2 = int(x0 - 1000*(-b))
+                        y2 = int(y0 - 1000*(a))
+
+                        cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+
+        # Create a combined image of Hough Line Transform result and the Canny Line Detector result.
+        combined = np.concatenate((img, cv.cvtColor(edges, cv.COLOR_GRAY2BGR)), axis=1)
+
+        cv.imshow('Hough Line Transform', combined)
+        cv.imshow('Dark', dark)
+
+        if cv.waitKey(1000) & 0xFF == ord('q'):
+            cv.destroyAllWindows()
+            break
+
+        return dark
 
 if __name__ == "__main__":
     main(sys.argv[1:])
