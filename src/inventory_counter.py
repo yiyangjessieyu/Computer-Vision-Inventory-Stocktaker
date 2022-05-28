786ded12cb14dbf5f1ddc18383446964fa5d85d4
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-05-28 16:58:29 +1200

    working tided single responsibility code

--- src/inventory_counter.py
+++ src/inventory_counter.py
@@ -1,71 +1,87 @@
 # hough_line.py
 
+
+# Imports needed for this program to run.
 import cv2 as cv
 import numpy as np
 
+
+# Global image files paths to set
 LOCAL_PATH = "/csse/users/yyu69/Desktop/COSC428/Project-april21/Computer-Vision-Inventory-Stocktaker/"
-INPUT_IMAGE_PATH = 'resources/side_tape.jpg'
+INPUT_IMAGE_PATH = 'resources/side_hearts.jpg'
 OUTPUT_IMAGE_PATH = './resources/dark.png'
+
+
+# Global hough normal settings to set.
+CANNY_THRESHOLD2 = 200
+HOUGH_THRESHOLD = 240
+
+
+def display_hough_window():
+    cv.namedWindow('Hough Line Transform')
+    cv.createTrackbar('CannyThreshold1', 'Hough Line Transform', 0, 1200, nothing)
+    cv.createTrackbar('CannyThreshold2', 'Hough Line Transform', CANNY_THRESHOLD2, 1200, nothing)
+    cv.createTrackbar("HoughThreshold", 'Hough Line Transform', HOUGH_THRESHOLD, 1200, nothing)
+
 
 def nothing(x):
     # We need a callback for the createTrackbar function.
     # It doesn't need to do anything, however.
     pass
 
-def extract_hough_normal(src):
 
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+def get_tracker_values():
+    canny_threshold1 = cv.getTrackbarPos('CannyThreshold1', 'Hough Line Transform')
+    canny_threshold2 = cv.getTrackbarPos('CannyThreshold2', 'Hough Line Transform')
+    hough_threshold = cv.getTrackbarPos('HoughThreshold', 'Hough Line Transform')
+    return canny_threshold1, canny_threshold2, hough_threshold
 
-    cv.namedWindow('Hough Line Transform')
-    cv.createTrackbar('CannyThreshold1', 'Hough Line Transform', 0, 1200, nothing)
-    cv.createTrackbar('CannyThreshold2', 'Hough Line Transform', 200, 1200, nothing)
-    cv.createTrackbar("HoughThreshold", 'Hough Line Transform', 240, 1200, nothing)
+def draw_hough_lines(lines):
+
+    # Create a new copy of the original image for drawing line detections on.
+    img = SOURCE_IMAGE.copy()
+
+    # Create a dark image for drawing line detections on and later line refine.
+    dark = np.zeros(img.shape)
+
+    if lines is not None:
+        for line in lines:
+            for rho, theta in line:
+                if (theta >= np.radians(0) and theta <= np.radians(10)) or \
+                    (theta >= np.radians(350) and theta <= np.radians(360)) or \
+                    (theta >= np.radians(170) and theta <= np.radians(190)):
+                    a = np.cos(theta)
+                    b = np.sin(theta)
+                    x0 = a * rho
+                    y0 = b * rho
+                    x1 = int(x0 + 1000 * (-b))
+                    y1 = int(y0 + 1000 * (a))
+                    x2 = int(x0 - 1000 * (-b))
+                    y2 = int(y0 - 1000 * (a))
+
+                    cv.line(dark, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    return img, dark
+
+
+
+def extract_hough_normal():
+    gray = cv.cvtColor(SOURCE_IMAGE, cv.COLOR_BGR2GRAY)
+    display_hough_window()
 
     while True:
-        houghThreshold = cv.getTrackbarPos('HoughThreshold', 'Hough Line Transform')
-        cannyThreshold1 = cv.getTrackbarPos('CannyThreshold1', 'Hough Line Transform')
-        cannyThreshold2 = cv.getTrackbarPos('CannyThreshold2', 'Hough Line Transform')
+        # Get the updated tracker values from the hough window.
+        canny_threshold1, canny_threshold2, hough_threshold = get_tracker_values()
 
-
-        # Create a new copy of the original image for drawing on later.
-        img = src.copy()
         # Use the Canny Edge Detector to find some edges.
-        edges = cv.Canny(gray, cannyThreshold1, cannyThreshold2)
+        edges = cv.Canny(gray, canny_threshold1, canny_threshold2)
+
         # Attempt to detect straight lines in the edge detected image.
-        lines = cv.HoughLines(edges, 1, np.pi/180, houghThreshold)
+        lines = cv.HoughLines(edges, 1, np.pi / 180, hough_threshold)
 
-        dark = np.zeros(img.shape)
-
-        # For each line that was detected, draw it on the img.
-        if lines is not None:
-            for line in lines:
-                for rho,theta in line:
-                    if (theta >= np.radians(0) and theta <= np.radians(10)) or \
-                        (theta >= np.radians(350) and theta <= np.radians(360)) or \
-                        (theta >= np.radians(170) and theta <= np.radians(190)) :
-                        a = np.cos(theta)
-                        b = np.sin(theta)
-                        x0 = a*rho
-                        y0 = b*rho
-                        x1 = int(x0 + 1000*(-b))
-                        y1 = int(y0 + 1000*(a))
-                        x2 = int(x0 - 1000*(-b))
-                        y2 = int(y0 - 1000*(a))
-
-                        cv.line(dark,(x1,y1),(x2,y2),(0,255,0),2)
-                        cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    # else:
-                    #     a = np.cos(theta)
-                    #     b = np.sin(theta)
-                    #     x0 = a*rho
-                    #     y0 = b*rho
-                    #     x1 = int(x0 + 1000*(-b))
-                    #     y1 = int(y0 + 1000*(a))
-                    #     x2 = int(x0 - 1000*(-b))
-                    #     y2 = int(y0 - 1000*(a))
-                    #
-                    #     cv.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
+        # Draw each line that was detected.
+        img, dark = draw_hough_lines(lines)
 
         # Create a combined image of Hough Line Transform result and the Canny Line Detector result.
         combined = np.concatenate((img, cv.cvtColor(edges, cv.COLOR_GRAY2BGR)), axis=1)
@@ -94,7 +110,7 @@ def read_image(image_to_read):
 
     # Check if image is loaded fine
     if src is None:
-        print ('Error opening image: ' + argv[0])
+        print ('Error opening image: ' + image_to_read)
         return -1
 
     # Scale the image down to 70% to fit on the monitor better.
@@ -102,13 +118,16 @@ def read_image(image_to_read):
 
     return src
 
+
 def main():
     # [load_image]
-    src = read_image(LOCAL_PATH + INPUT_IMAGE_PATH)
-    cv.imshow("src", src)
+    global SOURCE_IMAGE
+    SOURCE_IMAGE = read_image(LOCAL_PATH + INPUT_IMAGE_PATH)
+    cv.imshow("Source Image", SOURCE_IMAGE)
 
     # [hough lines]
-    dark = extract_hough_normal(src)
+    dark = extract_hough_normal()
+
 
 if __name__ == "__main__":
     main()
