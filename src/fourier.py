b9a0d18ccb7f71766046e5d70a2ee24149447773
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-06-02 10:02:10 +1200

    look into fourier

--- src/fourier.py
+++ src/fourier.py
@@ -1,79 +1,56 @@
-from __future__ import print_function
-import sys
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Global input file paths to set.
 LOCAL_PATH = "/csse/users/yyu69/Desktop/COSC428/Project-april21/Computer-Vision-Inventory-Stocktaker/resources/"
 INPUT_IMAGE_PATH = 'side_hearts_9.jpg'
 
-WINDOW_RATIO = 0.4
+img = cv.imread(LOCAL_PATH + INPUT_IMAGE_PATH,0)
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
-def print_help():
-    print('''
-    This program demonstrated the use of the discrete Fourier transform (DFT).
-    The dft of an image is taken and it's power spectrum is displayed.
-    Usage:
-    discrete_fourier_transform.py [image_name -- default lena.jpg]''')
+rows, cols = img.shape
+crow,ccol = rows//2 , cols//2
+fshift[crow-30:crow+31, ccol-30:ccol+31] = 0
+f_ishift = np.fft.ifftshift(fshift)
+img_back = np.fft.ifft2(f_ishift)
+img_back = np.real(img_back)
+plt.subplot(131),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
+plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+plt.subplot(133),plt.imshow(img_back)
+plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+plt.show()
 
+dft = cv.dft(np.float32(img),flags = cv.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
+magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
-def main():
-    print_help()
-
-    I = cv.imread("dilate.png", cv.IMREAD_GRAYSCALE)
-    if I is None:
-        print('Error opening image')
-        return -1
-
-    I = cv.resize(I, (int(I.shape[1] * WINDOW_RATIO), int(I.shape[0] * WINDOW_RATIO)))
-
-    # Expand the image to an optimal size
-    rows, cols = I.shape
-    m = cv.getOptimalDFTSize(rows)
-    n = cv.getOptimalDFTSize(cols)
-    padded = cv.copyMakeBorder(I, 0, m - rows, 0, n - cols, cv.BORDER_CONSTANT, value=[0, 0, 0])
-
-    # Make place for both the complex and the real values
-    planes = [np.float32(padded), np.zeros(padded.shape, np.float32)]
-    complexI = cv.merge(planes)  # Add to the expanded another plane with zeros
-    cv.dft(complexI, complexI)  # this way the result may fit in the source matrix
-
-    # Transform the real and complex values to magnitude
-    cv.split(complexI, planes)  # planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-    cv.magnitude(planes[0], planes[1], planes[0])  # planes[0] = magnitude
-    magI = planes[0]
-
-    # Switch to a logarithmic scale
-    matOfOnes = np.ones(magI.shape, dtype=magI.dtype)
-    cv.add(matOfOnes, magI, magI)  # switch to logarithmic scale
-    cv.log(magI, magI)
-
-    # Prepare to crop and rearrange
-    magI_rows, magI_cols = magI.shape
-
-    # Crop the spectrum, if it has an odd number of rows or columns
-    magI = magI[0:(magI_rows & -2), 0:(magI_cols & -2)]
-    cx = int(magI_rows / 2)
-    cy = int(magI_cols / 2)
-    q0 = magI[0:cx, 0:cy]  # Top-Left - Create a ROI per quadrant
-    q1 = magI[cx:cx + cx, 0:cy]  # Top-Right
-    q2 = magI[0:cx, cy:cy + cy]  # Bottom-Left
-    q3 = magI[cx:cx + cx, cy:cy + cy]  # Bottom-Right
-    tmp = np.copy(q0)  # swap quadrants (Top-Left with Bottom-Right)
-    magI[0:cx, 0:cy] = q3
-    magI[cx:cx + cx, cy:cy + cy] = tmp
-    tmp = np.copy(q1)  # swap quadrant (Top-Right with Bottom-Left)
-    magI[cx:cx + cx, 0:cy] = q2
-    magI[0:cx, cy:cy + cy] = tmp
-
-    # Normalize
-    cv.normalize(magI, magI, 0, 1, cv.NORM_MINMAX)  # Transform the matrix with float values into a
-
-    # Window details
-    cv.imshow("Input Image", I)  # Show the result
-    cv.imshow("spectrum magnitude", magI)
-    cv.waitKey()
-
-
-if __name__ == "__main__":
-    main()
+rows, cols = img.shape
+crow,ccol = rows/2 , cols/2
+# create a mask first, center square is 1, remaining all zeros
+mask = np.zeros((rows,cols,2),np.uint8)
+mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+# apply mask and inverse DFT
+fshift = dft_shift*mask
+f_ishift = np.fft.ifftshift(fshift)
+img_back = cv.idft(f_ishift)
+img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
